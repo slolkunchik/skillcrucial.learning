cc0a5adf2dcf822620d5b6d30e9a6bb16c10ef9d
Marina Sevkovich <slolkunchik@gmail.com>
2019-10-12 18:53:25 +0300

    feat: add new reduser

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, userEffect } from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import Head from './head'
+import { getData } from '../redux/reducers/users'
 
-const Dummy = () => {
+const Dummy = (props) => {
   const [counter] = useState(4)
+  const { getData: getDataProps } = props
+  userEffect(() => {
+    getDataProps();
+  }, [getDataProps])
+
   return (
     <div>
       <Head title="Hello" />
       <div> Hello World {counter} </div>
+      <div>{JSON.stringify(props.users)} </div>
       <img src={`/tracker/${counter}.gif`} alt="tracker" />
     </div>
   )
@@ -17,8 +24,10 @@ const Dummy = () => {
 
 Dummy.propTypes = {}
 
-const mapStateToProps = () => ({})
+const mapStateToProps = state => ({
+  user: state.user.list
+})
 
-const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ getData }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dummy)
